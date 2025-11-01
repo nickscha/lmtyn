9f94064a18d56fa4d075b88940a0d051ee613ab0
nickscha <nick@scharnbeck.de>
2025-11-01 11:39:13 +0100

    [VERSION] bump to v0.2

--- lmtyn.h
+++ lmtyn.h
@@ -1,4 +1,4 @@
-/* lmtyn.h - v0.1 - public domain data structures - nickscha 2025
+/* lmtyn.h - v0.2 - public domain data structures - nickscha 2025
 
 A C89 standard compliant, single header, nostdlib (no C Standard Library) Lucid Modelling Tool You Need (LMTYN).
 
